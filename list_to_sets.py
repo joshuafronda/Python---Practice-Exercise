b42df98367bf3d0aa2ef7875f32c105ccaf64fc5
joshuafronda <21-07245@g.batstate-u.edu.ph>
2025-10-04 23:43:19 +0800

    add sample functional

---
+++ list_to_sets.py
@@ -0,0 +1,13 @@
+#List to Set Conversion
+my_list = [9, 18, 27, 36, 45]
+my_set = set(my_list)
+print(my_set)
+# Output: {36, 9, 45, 18, 27}
+
+#Set to List Conversion
+my_set = {7, 14, 21, 28, 35}
+my_list = list(my_set)
+print(my_list)
+# Output: [35, 7, 14, 21, 28]  (order may vary)
+
+
